66c867344e19a7f97e14e27b745cb9f019467c5c
Eric <neueEKmail@web.de>
2021-01-25 16:43:06 +0100

    Register page

--- views/main/register.php
+++ views/main/register.php
@@ -1,4 +1,4 @@
-<div>
+<!--<div>
 	<form action = 'index.php?c=main&a=register' method = 'POST'>
 
 
@@ -64,7 +64,7 @@
 			<input id="password"
 			       name="password"
 			       type="password"
-			       placeholder="passowrd"
+			       placeholder="password"
 			       required value=<?php echo (isset($_POST['password']) ? $_POST['password'] : ''); //default Values?> >
 		</div>
 		<br>
@@ -73,4 +73,72 @@
 		</div>
 	</form>
 
-</div>
+</div>-->
+
+
+<!DOCTYPE html>
+
+<html>
+    <!--<head>
+        <link rel="stylesheet" href="menuStyles.css">
+        <link rel="stylesheet" href="Styles.css">
+        <link rel="stylesheet" href="topBottom.css">
+        <link rel="stylesheet" href="LogReg.css">
+    </head>-->
+
+    <body>
+        <div class = "InputBox">
+	        <form action = 'index.php?c=main&a=register' method = 'POST'>
+
+            <h3>Registrierung</h3>
+            <p>
+                <div class = "InputLine">
+                    <input id="username"
+                           name="username"
+                           type="username"
+                           placeholder="Username"
+                           required value=<?php echo (isset($_POST['username']) ? $_POST['username'] : ''); //default Values?> >
+                    </br>
+
+                    <input id="password"
+                           name="password"
+                           type="password"
+                           placeholder="Password"
+                           required value=<?php echo (isset($_POST['password']) ? $_POST['password'] : ''); //default Values?> >
+                </div>
+            </p>
+            <p>
+                <div class = "InputLine">
+                    <input id="firstName"
+                           name="firstName"
+                           type="firstName"
+                           placeholder="First name"
+                           required value=<?php echo (isset($_POST['firstName']) ? $_POST['firstName'] : ''); //default Values?> >
+                    </br>
+                    <input id="lastName"
+                           name="lastName"
+                           type="lastName"
+                           placeholder="Last name"
+                           required value=<?php echo (isset($_POST['lastName']) ? $_POST['lastName'] : NULL); //default Values?> >
+
+                    <p></p>
+
+                    <input id="emailAddress"
+                           name="emailAddress"
+                           type="email"
+                           placeholder="e-Mail"
+                           required value=<?php echo (isset($_POST['emailAddress']) ? $_POST['emailAddress'] : NULL); //default Values?> >
+                    </br>
+                    <input id="phoneNumber"
+                           name="phoneNumber"
+                           type="tel"
+                           placeholder="Mobile number"
+                           value=<?php echo (isset($_POST['phoneNumber']) ? $_POST['phoneNumber'] : ''); //default Values?> >
+                </div>
+            </p>
+
+            <p><input type = "checkbox"> AGB</p>
+            <p><input type = "submit" value = "Registrieren"/></p>
+        </div>
+    </body>
+</html>
