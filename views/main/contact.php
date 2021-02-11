52c05d79d018fb8b462b7c8f53d06f3b4668a5d6
Eric <neueEKmail@web.de>
2021-02-11 17:53:27 +0100

    Minor changes about fonts

--- views/main/contact.php
+++ views/main/contact.php
@@ -1,1 +1,23 @@
-<?php
+<div class = "page">
+    <div class="HeaderBox">
+        <h1>Kontakt</h1>
+    </div>
+
+    <div class="page">
+        <div class="TextBox">
+            Wir sind erreichbar unter:<br>
+            <br>
+            E-Mail: info@DDDrueckler.com<br>
+            Tel.: 01520 08151337<br>
+            <br>
+            Fuchsstraße 66<br>
+            99642 Strullerdorf
+        </div>
+
+        <!--Horizontal divided flexboxes-->
+
+        <!--<div class="TextBox">
+            Contact Formular if necessary-
+        </div>-->
+    </div>
+</div>
