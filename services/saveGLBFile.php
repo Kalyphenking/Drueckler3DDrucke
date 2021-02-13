8c04b5282668b734cd10f86ba0c87589d7cdbda5
Marius Lange <bb.m.lange19@gmail.com>
2021-02-13 18:35:08 +0100

    Update

--- services/saveGLBFile.php
+++ services/saveGLBFile.php
@@ -2,7 +2,6 @@
 
 $error = "";
 
-$myfile = fopen("../log/stlErrors.txt", "w") or die("Unable to open file!");
 
 if (isset($_FILES['file']) && !empty($_FILES['file']))
 {
@@ -10,14 +9,12 @@ if (isset($_FILES['file']) && !empty($_FILES['file']))
 
 		$fileName = trim($_POST['fileName'], '"');
 
-		$txt = 'fileName: '.$fileName . "\n\n";
-		fwrite($myfile, $txt);
+
 
 
 		$glbDirectory = substr($fileName, 0, strpos($fileName, 'glb')+3);
 
-		$txt = 'glbDirectory: '.$glbDirectory . "\n\n";
-		fwrite($myfile, $txt);
+
 
 		$files = scandir('..'.DIRECTORY_SEPARATOR.$glbDirectory, SCANDIR_SORT_NONE);
 //		$newest_file = $files[0];
@@ -26,22 +23,19 @@ if (isset($_FILES['file']) && !empty($_FILES['file']))
 
 		$modelName = substr($modelName, 0, strlen($modelName) - 7);
 
-		$txt = 'modelName: ' .$modelName. "\n\n";
-		fwrite($myfile, $txt);
+
 
 		foreach ($files as $file) {
 			$extensiont = substr($file, strlen($file) - 3, 3);
 			$baseName = substr($file, 0, strlen($file)-7);
 
-			$txt = 'baseName: ' .$baseName. "\n\n";
-			fwrite($myfile, $txt);
+
 
 			$deleteName = trim($file, '"');
 
 
 			if (($extensiont == 'glb') && ($modelName === $baseName)) {
-				$txt = 'deleteName: ' .$deleteName. "\n\n";
-				fwrite($myfile, $txt);
+
 				unlink('..'.DIRECTORY_SEPARATOR.$glbDirectory.DIRECTORY_SEPARATOR.$deleteName);
 			}
 
@@ -50,8 +44,7 @@ if (isset($_FILES['file']) && !empty($_FILES['file']))
 
 
 		if (move_uploaded_file($_FILES['file']['tmp_name'], '..'.DIRECTORY_SEPARATOR.$fileName)) {
-			$txt = 'saved' . "\n\n";
-			fwrite($myfile, $txt);
+
 		} else {
 			echo 'Moglicherweise shit\n';
 			$error .= 'Moglicherweise shit\n';
@@ -63,8 +56,28 @@ if (isset($_FILES['file']) && !empty($_FILES['file']))
 	}
 }
 
-
-$txt = 'works' . "\n\n";
-fwrite($myfile, $txt);
-
-fclose($myfile);
+//$myfile = fopen("../log/stlErrors.txt", "w") or die("Unable to open file!");
+//
+//
+//$txt = 'fileName: '.$fileName . "\n\n";
+//fwrite($myfile, $txt);
+//
+//$txt = 'modelName: ' .$modelName. "\n\n";
+//fwrite($myfile, $txt);
+//
+//$txt = 'glbDirectory: '.$glbDirectory . "\n\n";
+//fwrite($myfile, $txt);
+//
+//$txt = 'baseName: ' .$baseName. "\n\n";
+//fwrite($myfile, $txt);
+//
+//$txt = 'deleteName: ' .$deleteName. "\n\n";
+//fwrite($myfile, $txt);
+//
+//$txt = 'saved' . "\n\n";
+//fwrite($myfile, $txt);
+//
+//$txt = 'works' . "\n\n";
+//fwrite($myfile, $txt);
+//
+//fclose($myfile);
