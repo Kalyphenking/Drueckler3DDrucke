c27b360e015de6e891c77b17a8f592fc603243e7
Marius Lange <bb.m.lange19@gmail.com>
2021-02-19 22:43:52 +0100

    DIRECTORY_SEPARATOR weg

--- config/paths.php
+++ config/paths.php
@@ -2,18 +2,18 @@
 
 // setup pahts to directories
 
-define('ROOTPATH', strlen(dirname($_SERVER['SCRIPT_NAME'])) > 1 ? dirname($_SERVER['SCRIPT_NAME']) . DIRECTORY_SEPARATOR : DIRECTORY_SEPARATOR);
-define('JSPATH',  'js'.DIRECTORY_SEPARATOR);
-define('CSSPATH',  'css'.DIRECTORY_SEPARATOR);
-define('COREPATH',  'core'.DIRECTORY_SEPARATOR);
-define('VIEWSPATH',  'views'.DIRECTORY_SEPARATOR);
-define('FUNCTIONSPATH', 'controller'.DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR);
-define('CONTROLLERSPATH',  'controller'.DIRECTORY_SEPARATOR);
-define('MODELSPATH',  'models'.DIRECTORY_SEPARATOR);
-define('IMAGESPATH',  'ressources'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR);
-define('UPLOADSPATH',  'uploads'.DIRECTORY_SEPARATOR);
+define('ROOTPATH', strlen(dirname($_SERVER['SCRIPT_NAME'])) > 1 ? dirname($_SERVER['SCRIPT_NAME']) . '/' : '/');
+define('JSPATH',  'js'.'/');
+define('CSSPATH',  'css'.'/');
+define('COREPATH',  'core'.'/');
+define('VIEWSPATH',  'views'.'/');
+define('FUNCTIONSPATH', 'controller'.'/'.'functions'.'/');
+define('CONTROLLERSPATH',  'controller'.'/');
+define('MODELSPATH',  'models'.'/');
+define('IMAGESPATH',  'ressources'.'/'.'images'.'/');
+define('UPLOADSPATH',  'uploads'.'/');
 
 
-//define('STL2GLTFPATH',  'stl2gltf'.DIRECTORY_SEPARATOR);
+//define('STL2GLTFPATH',  'stl2gltf'.'/');
 
-//define('SLICER', 'Prusa_Slicer'.DIRECTORY_SEPARATOR.'buildScript.sh');
+//define('SLICER', 'Prusa_Slicer'.'/'.'buildScript.sh');
